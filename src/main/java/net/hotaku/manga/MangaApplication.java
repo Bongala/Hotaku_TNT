1e25ee6bc10f2f3219d047a57b2ac172ea095cd6
ngN-tieN <82561495+ngN-tieN@users.noreply.github.com>
2025-04-19 17:51:54 +0700

    Sync (#8)
    * [Add]gitignore, [Update] properties file, Maven file
    
    * [Add] Maven Wrapper
    
    * [Add] Spring boot starter web
    
    * Add gitignore
    
    * [Feat] Re-init with dockercompose
    
    * [Feat] Add pgadmin & docker comppose (#2)
    
    * Infra: Setup git action with sonarless and dockerize
    
    * fix action file url
    
    * fix jdk version and sonarless fail resovler
    
    * Infra: Setup git action with sonarless and dockerize (#3)
    
    * Infra: Setup git action with sonarless and dockerize
    
    * fix action file url
    
    * fix jdk version and sonarless fail resovler
    
    * fix: add build command to analyze
    
    * Revert "Infra: Setup git action with sonarless and dockerize (#3)"
    
    This reverts commit a0a21e95fbfd25678d3efb2fb2cffffa41554897.
    
    * fix: add build command to analyze
    
    * fix: add build command to analyze
    
    * fix: remove scan
    
    * fix add dockerfile to push to dockerhub
    
    ---------
    
    Co-authored-by: luxion-rob <aidaynhi8@gmail.com>
    Co-authored-by: Bui Cong Tri <45875394+luxion-rob@users.noreply.github.com>

--- src/main/java/net/hotaku/manga/MangaApplication.java
+++ src/main/java/net/hotaku/manga/MangaApplication.java
@@ -1,11 +1,12 @@
-package com.hotaku.manga;
+package net.hotaku.manga;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,	// TODO: Delete when implement authenticate feature
+})
 public class MangaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MangaApplication.class, args);
 	}
